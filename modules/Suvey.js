feee9e076cfa960f9ad740b1cc335b462f3a023b
dkxltks25 <dkxltks25@naver.com>
2019-12-20 19:51:52 +0900

    function not change

--- modules/Suvey.js
+++ modules/Suvey.js
@@ -3,17 +3,25 @@
 function Survey (container){
     this.Survey = container
     this.SurveyName = "Survey";
-    this.SurveyItemName = "";
+    this.SurveyTitle ="SurveyTitle";
+    this.SurveyItemName = "SurveyItemName";
     this.SurveyItemsTools = "SurveyItemTools";
     this.SurveyItemSubjectName = "";
     this.SurveyItemContentName = "";
 }
+
+//설문지 
+Survey.prototype.createSurveyTitle = function(){
+    const SurveyTitle = CreateDivTag();
+    console.dir(SurveyTitle);
+
+    
+}
 //항목 생성
 Survey.prototype.createSurveyItem = function(){
     const SurveyItem = document.createElement("div");
-    SurveyItem.classList.add(SurveyItemName);
-
-
+    SurveyItem.classList.add(this.SurveyItemName);
+    this.Survey.appendChild(SurveyItem);
 }   
 //항목 관리 도구
 Survey.prototype.createSurveyTools = function(){
@@ -21,8 +29,16 @@ Survey.prototype.createSurveyTools = function(){
     SurveyItem.classList.add(SurveyItemsTools);
 }
 
+
+// 공통 태그 생성 영역
+const CreateDivTag = (divName,divTextNode) =>{
+    const DivTag = document.createElement("div");
+    DivTag.classList.add(divName);
+    DivTag.classList.add("mui-panel");
+    return DivTag
+}
 // 제일 처음 실행어야 하는 함수
 Survey.prototype.createSurvey = function(){
-
-
+    this.createSurveyItem();
+    this.createSurveyTitle();
 }
