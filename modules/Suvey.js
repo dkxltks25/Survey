577e6e22f900b68168a9d38561c8d2409a57f958
dkxltks25 <dkxltks25@naver.com>
2019-12-21 15:02:40 +0900

    function not change update function [1],[2]

--- modules/Suvey.js
+++ modules/Suvey.js
@@ -9,30 +9,54 @@ function Survey (container){
     this.SurveyItemSubjectName = "";
     this.SurveyItemContentName = "";
     
+    this.SurveyTitleInput = "SurveyTitleInput";
+    this.SurveyDescripInput = "SurveyDescripInput";
+
     //아이콘 이름
     //추가 버튼
     this.IconAddName = "add";
     this.ButtonIconAddName = "IconAdddButton";
-
+    
     //material 태그 적용
     this.MaterialPanel = "mui-panel";
+    this.MaterialInput = "input-field";
+    this.MaterialTextArea = "materialize-textarea";
 }
 
-//설문지 
+//설문지 제목 생성
 Survey.prototype.createSurveyTitle = function(){
     const SurveyTitle = createDivTag([this.SurveyTitle,this.MaterialPanel]);
+    
+    const InputTitle = createDivTag([this.SurveyTitleInput,this.MaterialInput]);
+    let placeholder = "제목";
+    const InputTag = createInput(null,"text",null,placeholder);
+    InputTitle.appendChild(InputTag);
+
+    placeholder = "설명";
+    const TextAreaTitle = createDivTag([this.SurveyDescripInput,this.MaterialInput]);
+    const TextAreaTag= createTextArea([this.MaterialTextArea],placeholder);
+    TextAreaTitle.appendChild(TextAreaTag);
+    
+    SurveyTitle.appendChild(InputTitle);        
+    SurveyTitle.appendChild(TextAreaTitle);
     this.Survey.appendChild(SurveyTitle);
+    
 }
 //항목 생성
-Survey.prototype.createSurveyItem = function(){
-    const SurveyItem = createDivTag([this.SurveyItemName,this.MaterialPanel]);
-    this.Survey.appendChild(SurveyItem);
+Survey.prototype.createSurveyItem = function(event,ReduceSurvey){
+  
 }   
 //항목 관리 도구
 Survey.prototype.createSurveyTools = function(){
     const SurveyTools = createDivTag([this.SurveyItemsTools,this.MaterialPanel]);
     //항목 관리 도구 Item 추기
+    const ReduceSurvey= this;
     const IconAddButton = createIconButton(this.ButtonIconAddName,this.IconAddName);
+    IconAddButton.addEventListener('click',()=>{
+        const SurveyItem = createDivTag([ReduceSurvey.SurveyItemName,ReduceSurvey.MaterialPanel]);
+        this.Survey.appendChild(SurveyItem);
+    });
+
     SurveyTools.appendChild(IconAddButton); 
     this.Survey.appendChild(SurveyTools);
 
@@ -44,7 +68,6 @@ Survey.prototype.createSurveyTools = function(){
 const createDivTag = (divName) =>{
     const DivTag = document.createElement("div");
     divName.map((index)=>DivTag.classList.add(index));
-    console.log(DivTag);
     return DivTag
 }
 const createIconButton = (IconButtonName,IconText)=>{
@@ -59,13 +82,20 @@ const createIconButton = (IconButtonName,IconText)=>{
     return IconButton;
 }
 
-const createInput = (InputName,type="text",text) =>{
+const createInput = (InputName,type="text",text,placeholder="입력해주세요") =>{        
     //input TextName을 생성합시당
-
+    console.log(placeholder);
     const InputTag = document.createElement("input");
     InputTag.setAttribute("type",type);
+    InputTag.setAttribute("placeholder",placeholder);
     InputTag.classList.add(InputName);
     return InputTag;
+}
+const createTextArea = (TextAreaName,placeholder) =>{
+    const TextAreaTag = document.createElement("textarea");
+    TextAreaTag.setAttribute("placeholder",placeholder);
+    TextAreaName.map((index=>TextAreaTag.classList.add(index)));
+    return TextAreaTag;
 }
 // 제일 처음 실행어야 하는 함수
 Survey.prototype.createSurvey = function(){
